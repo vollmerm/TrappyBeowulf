1743dd554c268f17c8bebca771ffb0b03f37fd6a
Mike Vollmer <mike@talkingincircles.net>
2013-08-12 19:46:31 -0700

    changed params

--- trappy.h
+++ trappy.h
@@ -12,15 +12,15 @@
 #define TRAPPY_DEBUG    0      // show weird debug stuff
 #define WRITE_TOP_TRAP  1      // show top-level traps
 #define TRAPPY          1      // enable trappy minimax
-#define MAX_DEPTH       6
-#define TRAP_METHOD     3      // Methods: 1. median, 2. best value, 3. last value
+#define MAX_DEPTH       7
+#define TRAP_METHOD     1      // Methods: 1. median, 2. best value, 3. last value
 #define TRAP_SCALE 	1
 //#define NO_AB
 //#define NO_ORDER
 #define TRAP_CEILING    2    // Trap evaluations scaled to never exceed a percentage
 			       // of the best guaranteed evaluation.
 #define TRAP_KEY_SIZE   151
-#define MAX_TRAP_DEPTH  3
+#define MAX_TRAP_DEPTH  6
 float trappiness(int M, const int * Scores, int scoreCount, int ply);
 float scale(float T, int M);
 void writeTrapData(int, int);
